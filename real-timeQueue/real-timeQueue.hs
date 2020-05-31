7ca0e1faf5c52d1f815f8c3ea8c215971a20f344
BlackNesta <andreimanaila99@gmail.com>
2020-06-01 02:40:21 +0300

    mai usor de manipulat

--- real-timeQueue/real-timeQueue.hs
+++ real-timeQueue/real-timeQueue.hs
@@ -1,32 +1,35 @@
--- | Abstracts the implementation details of a single-insertion, single-extraction queuelike structure with amortised complexity.
 module Real.Time.Queue where
-data Queue a = Queue [a] [a] deriving (Show, Eq)
+-- | Implementarea unei cozi cu complexitate amortizata
+data Queue a = Queue [a] [a] deriving(Show)
 
--- | A generic type class encapsulating a generic queuelike structure, that supports single-insertion and single-extraction
-class QueueOperations q where
-    -- | Constructs an empty queue.
-    newQ :: q a
-    -- | Checks if the queue is empty.
-    empty :: q a -> Bool
-    -- | Inserts a single element into the 'Queue'.
-    push :: q a -> a -> q a
-    -- | Attempts to extract an element from the 'Queue'. 'Queue' must not be empty!
-    pop :: q a -> q a
-    -- | Gets the element that will next be extracted from the 'Queue'. 'Queue' must not be empty!
-    front :: q a -> a
-    -- | Returns the elements of the 'Queue' in reverse order.
-    rev :: q a -> q a
-    -- | Gets the size of the queue
-    size :: q a -> Int
+instance Eq a => Eq (Queue a) where
+    (==) (Queue [] []) (Queue [] []) = True
+    (==) q1 q2 = do
+        if (size q1) /= (size q2) then False
+        else if (front q1) /= (front q2) then False
+        else (==) (pop q1) (pop q2) 
 
-instance QueueOperations Queue where
-    empty (Queue [] []) = True
-    empty _ = False
-    newQ = Queue [] []
-    push (Queue xs ys) x = Queue xs (x : ys)
-    pop (Queue [] ys) = pop (Queue (reverse ys) [])
-    pop (Queue (x : xs) ys) = Queue xs ys
-    front (Queue [] ys) = front (Queue (reverse ys) [])
-    front (Queue (x : xs) ys) = x
-    rev (Queue xs ys) = Queue ys xs
-    size (Queue xs ys) = (length xs) + (length ys)
+-- | Constructs an empty queue.
+newQ :: Queue a
+newQ = Queue [] []
+-- | Checks if the queue is empty.
+empty :: Queue a -> Bool
+empty (Queue [] []) = True
+empty _ = False
+-- | Inserts a single element into the 'Queue'.
+push :: Queue a -> a -> Queue a 
+push (Queue xs ys) x = Queue xs (x : ys)
+-- | Attempts to extract an element from the 'Queue'. 'Queue' must not be empty!
+pop :: Queue a -> Queue a
+pop (Queue [] ys) = pop (Queue (reverse ys) [])
+pop (Queue (x : xs) ys) = Queue xs ys
+-- | Gets the element that will next be extracted from the 'Queue'. 'Queue' must not be empty!front :: Queue a -> a
+front (Queue [] ys) = front (Queue (reverse ys) [])
+front (Queue (x : xs) ys) = x
+-- | Returns the elements of the 'Queue' in reverse order.
+rev :: Queue a -> Queue a
+rev (Queue xs ys) = Queue ys xs
+-- | Gets the size of the queue
+size :: Queue a -> Int
+size (Queue xs ys) = (length xs) + (length ys)
+
